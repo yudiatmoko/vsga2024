5253f92697ef8467ec06c9679865cfc73dd29965
yudiatmoko <yudiatmokoilham@gmail.com>
2024-07-22 12:43:36 +0700

    Finishing Name Input Page

--- app/src/main/java/com/iyam/myvsgaproject2/MainActivity.java
+++ app/src/main/java/com/iyam/myvsgaproject2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.iyam.myvsgaproject2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.iyam.myvsgaproject2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    EditText nameInput;
+    TextView nameTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nameInput = binding.etName;
+        nameTitle = binding.tvUsernameTitle;
+    }
+
+    public void showName(View view) {
+        nameTitle.setText(getString(R.string.your_name_text, nameInput.getText()));
     }
 }
