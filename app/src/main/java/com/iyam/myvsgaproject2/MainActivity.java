3d6e536aeab35ae940f52be427097305e8442094
yudiatmoko <yudiatmokoilham@gmail.com>
2024-07-31 09:45:35 +0700

    Finishing layout

--- app/src/main/java/com/iyam/myvsgaproject2/MainActivity.java
+++ app/src/main/java/com/iyam/myvsgaproject2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.iyam.myvsgaproject2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.iyam.myvsgaproject2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        binding = ActivityMainBinding.inflate(
+                getLayoutInflater(),
+                getWindow().getDecorView().findViewById(android.R.id.content),
+                false
+        );
+        fab = binding.floatingActionButton;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setOnClickListener();
+    }
+
+    private void setOnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddNewActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
