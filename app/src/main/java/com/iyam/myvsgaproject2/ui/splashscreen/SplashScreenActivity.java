284d6c4cab7385027343172e3640d8b531081cbc
yudiatmoko <yudiatmokoilham@gmail.com>
2024-08-10 02:11:19 +0700

    Create login and register layout

---
+++ app/src/main/java/com/iyam/myvsgaproject2/ui/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,48 @@
+package com.iyam.myvsgaproject2.ui.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.iyam.myvsgaproject2.databinding.ActivitySplashScreenBinding;
+import com.iyam.myvsgaproject2.ui.login.LoginActivity;
+import com.iyam.myvsgaproject2.ui.main.MainActivity;
+import com.iyam.myvsgaproject2.ui.register.RegisterActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ActivitySplashScreenBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        binding = ActivitySplashScreenBinding.inflate(
+            getLayoutInflater(),
+            getWindow().getDecorView().findViewById(android.R.id.content),
+            false
+        );
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        navigateToMain();
+    }
+
+    private void navigateToMain() {
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        }, 2000);
+    }
+}
