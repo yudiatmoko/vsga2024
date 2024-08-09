99d6af7b317f562bde10f176269f4f245fb122ff
yudiatmoko <yudiatmokoilham@gmail.com>
2024-08-09 11:39:48 +0700

    Light sensor practice

--- app/src/main/java/com/iyam/myvsgaproject2/MainActivity.java
+++ app/src/main/java/com/iyam/myvsgaproject2/MainActivity.java
@@ -1,6 +1,13 @@
 package com.iyam.myvsgaproject2;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,68 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.iyam.myvsgaproject2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ActivityMainBinding binding;
+    private TextView tvLightSensor;
+    private ImageView ivEmoticon;
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        binding = ActivityMainBinding.inflate(
+            getLayoutInflater(),
+            getWindow().getDecorView().findViewById(android.R.id.content),
+            false
+        );
+        tvLightSensor = binding.tvLightSensor;
+        ivEmoticon = binding.ivEmoticon;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setSensorLight();
+    }
+
+    private void setSensorLight() {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float lux = sensorEvent.values[0];
+        tvLightSensor.setText(getString(R.string.light_sensor, lux));
+        if (lux > 60){
+            ivEmoticon.setImageResource(R.drawable.baseline_mood);
+        } else {
+            ivEmoticon.setImageResource(R.drawable.baseline_mood_bad);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
