e8780162c21422b0448182c43cbc11baa50631c9
yudiatmoko <yudiatmokoilham@gmail.com>
2024-08-13 02:12:28 +0700

    Create layout detail note

--- app/src/main/java/com/iyam/myvsgaproject2/ui/main/MainActivity.java
+++ app/src/main/java/com/iyam/myvsgaproject2/ui/main/MainActivity.java
@@ -10,27 +10,35 @@
 
 package com.iyam.myvsgaproject2.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.iyam.myvsgaproject2.R;
 import com.iyam.myvsgaproject2.data.preferences.Preferences;
 import com.iyam.myvsgaproject2.databinding.ActivityMainBinding;
+import com.iyam.myvsgaproject2.ui.detail.DetailActivity;
+import com.iyam.myvsgaproject2.ui.profile.ProfileActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private Button btnLogout;
-    private TextView tvUsername;
+    private FloatingActionButton fab;
+    private RecyclerView rvNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +47,8 @@ public class MainActivity extends AppCompatActivity {
                 getWindow().getDecorView().findViewById(android.R.id.content),
                 false
         );
-        btnLogout = binding.button;
-        tvUsername = binding.tvHelloWorld;
+        fab = binding.fab;
+        rvNotes = binding.rvNotes;
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -55,14 +63,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setOnClickListener() {
-        btnLogout.setOnClickListener(view -> {
-            Preferences.clearLoggedInUser(getBaseContext());
-            finish();
-        });
+        fab.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, DetailActivity.class)));
     }
 
     private void getUserPreferences() {
-        String username = Preferences.getLoggedInUser(this);
-        tvUsername.setText(username);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.profile) {
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+        }
+        return true;
     }
 }
